a36f081a3ea882d826d1c7d854fdc7fc7e88f854
21P31A0547 <21P31A0547@acet.ac.in>
2024-03-18 21:52:33 +0530

    com

---
+++ arr.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class arr
+{
+public static void main(String args[])
+{
+int arr[]=new int[n];
+int arre[];
+int arro[];
+int n;
+System.out.println("enter n");
+n=sc.nextInt();
+for(int i=0;i<arr.length;i++)
+{
+ arr[i]=sc.nextInt();
+}
+for(i=0;i<n;i++)
+{
+if(arr[i]%2==0)
+{
+ arre[]=arr[];
+}
+}
+for(i=0;i<n;i++)
+{
+if(arr[i]%2!=0)
+{
+ arro[]=arr[i];
+}
+}
+System.out.println(arre[]);
+System.out.println(arro[]);
+}
+}
+
+
